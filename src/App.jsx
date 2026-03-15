67db65d8ddebc6501dd8d1a044ac2ae0e62f56a4
acurzon <adam@curzon.org>
2026-03-15 13:29:34 +0000

    feat: store inputs in local storage, and styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,101 +4,113 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const PROGRAM_WEEKS = 3;
-  const DAYS_PER_WEEK = 4;
-  const PERCENT_WEIGHT_EACH_DAY = [0.70, 0.75, 0.80, 0.85];
-  const REPS_EACH_DAY = [6, 5, 4, 3];
-  const SETS_EACH_DAY = [6, 7, 8, 10];
+    const PROGRAM_WEEKS = 3;
+    const DAYS_PER_WEEK = 4;
+    const PERCENT_WEIGHT_EACH_DAY = [0.70, 0.75, 0.80, 0.85];
+    const REPS_EACH_DAY = [6, 5, 4, 3];
+    const SETS_EACH_DAY = [6, 7, 8, 10];
 
-  const [repMax, setRepMax] = useState(63);
-  const [increment, setIncrement] = useState(5);
-  const [weeks, setWeeks] = useState([]);
+    const DEFUALT_REPMAX = 60;
+    const DEFAULT_INCREMENT = 5;
 
-  function updatePlan() {
-    if (!increment || !repMax) {
-      return;
+    const [repMax, setRepMax] = useState(60);
+    const [increment, setIncrement] = useState(5);
+    const [weeks, setWeeks] = useState([]);
+
+    function updatePlan() {
+        if (!increment || !repMax) {
+            return;
+        }
+        const newWeeks = [];
+
+        for (let week = 0; week < PROGRAM_WEEKS; week++) {
+            const newWeek = [];
+            for (let day = 0; day < DAYS_PER_WEEK; day++) {
+                newWeek.push({
+                    day: day + 1,
+                    reps: REPS_EACH_DAY[day],
+                    sets: SETS_EACH_DAY[day],
+                    // weight: PERCENT_WEIGHT_EACH_DAY[day] * repMax + (increment * week)
+                    weight: Math.round((PERCENT_WEIGHT_EACH_DAY[day] * repMax + (increment * week)) / 2.5) * 2.5
+                });
+            }
+            newWeeks.push(newWeek);
+        }
+
+        setWeeks(newWeeks);
     }
-    const newWeeks = [];
 
-    for (let week = 0; week < PROGRAM_WEEKS; week++) {
-      const newWeek = [];
-      for (let day = 0; day < DAYS_PER_WEEK; day++) {
-        newWeek.push({
-          day: day + 1,
-          reps: REPS_EACH_DAY[day],
-          sets: SETS_EACH_DAY[day],
-          // weight: PERCENT_WEIGHT_EACH_DAY[day] * repMax + (increment * week)
-          weight: Math.round((PERCENT_WEIGHT_EACH_DAY[day] * repMax + (increment * week)) / 2.5) * 2.5
-        });
-      }
-      newWeeks.push(newWeek);
+    function handleRepMaxChange(e) {
+        setRepMax(e.target.value);
+        localStorage.setItem("repMax", e.target.value);
     }
 
-    setWeeks(newWeeks);
-  }
-
-  function handleRepMaxChange(e) {
-    setRepMax(e.target.value);
-  }
-
-  function handleIncrementChange(e) {
-    setIncrement(e.target.value);
-  }
+    function handleIncrementChange(e) {
+        setIncrement(e.target.value);
+        localStorage.setItem("increment", e.target.value);
+    }
 
 
-  useEffect(() => {
-    updatePlan();
-  }, [increment, repMax]);
+    useEffect(() => {
+        setRepMax(localStorage.getItem("repMax") ?? DEFUALT_REPMAX);
+        setIncrement(localStorage.getItem("increment") ?? DEFAULT_INCREMENT);
+        updatePlan();
+    }, []);
 
-  return (
-    <>
-      <h1>React Overload</h1>
-      <div className="inputs">
-        <div className="input_row">
-          <label for="rep_max">1 Rep Max</label>
-          <input
-            type="text"
-            id="rep_max"
-            value={repMax}
-            onChange={handleRepMaxChange} />
-        </div>
-        <div className="input_row">
-          <label for="increment">Increment</label>
-          <select id="increment"
-            onChange={handleIncrementChange} >
-            <option value="2.5">2.5</option>
-            <option value="5" selected>5</option>
-            <option value="7.5">7.5</option>
-          </select>
-        </div>
-      </div >
-      <div className="outputs">
-        {weeks.map((week, w) => <div className="week" key={w}>
-          <h2>Week {w + 1}</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Day</th>
-                <th>Sets</th>
-                <th>Reps</th>
-                <th>Weight</th>
-              </tr>
-            </thead>
-            <tbody>
-              {week.map((day, d) =>
-                <tr key={d}>
-                  <td>{day.day}</td>
-                  <td>{day.sets}</td>
-                  <td>{day.reps}</td>
-                  <td>{day.weight}</td>
-                </tr>)}
-            </tbody>
-          </table>
-        </div>)}
+    useEffect(() => {
+        updatePlan();
+    }, [increment, repMax]);
 
-      </div>
-    </>
-  )
+    return (
+        <>
+            <h2>React Overload</h2>
+            <div className="inputs">
+                <div className="input_row">
+                    <label htmlFor="rep_max">1 Rep Max</label>
+                    <input
+                        type="text"
+                        id="rep_max"
+                        value={repMax}
+                        onChange={handleRepMaxChange} />
+                </div>
+                <div className="input_row">
+                    <label htmlFor="increment">Increment</label>
+                    <select id="increment"
+                        onChange={handleIncrementChange}
+                        defaultValue={5} >
+                        <option value="2.5">2.5</option>
+                        <option value="5">5</option>
+                        <option value="7.5">7.5</option>
+                    </select>
+                </div>
+            </div >
+            <div className="outputs">
+                {weeks.map((week, w) => <div className="week" key={w}>
+                    <h2>Week {w + 1}</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Day</th>
+                                <th>Sets</th>
+                                <th>Reps</th>
+                                <th>Weight</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {week.map((day, d) =>
+                                <tr key={d}>
+                                    <td>{day.day}</td>
+                                    <td>{day.sets}</td>
+                                    <td>{day.reps}</td>
+                                    <td>{day.weight}</td>
+                                </tr>)}
+                        </tbody>
+                    </table>
+                </div>)}
+
+            </div>
+        </>
+    )
 }
 
 export default App
